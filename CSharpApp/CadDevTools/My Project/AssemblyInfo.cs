79a294479d7bd60b9ba28d5377a008c9c62a0c7d
cadwiki <99513485+cadwiki@users.noreply.github.com>
2022-09-09 13:49:30 -0700

    reset AssemblyInfo files

--- CSharpApp/CadDevTools/My Project/AssemblyInfo.cs
+++ CSharpApp/CadDevTools/My Project/AssemblyInfo.cs
@@ -52,5 +52,5 @@ using System.Windows;
 // by using the '*' as shown below:
 // <Assembly: AssemblyVersion("1.0.*")>
 
-[assembly: AssemblyVersion("1.0.0909.027")]
-[assembly: AssemblyFileVersion("1.0.0909.027")]
+[assembly: AssemblyVersion("1.0.0909.000")]
+[assembly: AssemblyFileVersion("1.0.0909.000"]
