62c7b28b2c9986525db2dac285e4449561998f14
cadwiki <99513485+cadwiki@users.noreply.github.com>
2022-09-25 11:04:30 -0700

    refactor MainWindow to use new DevTools constructor

--- VbNetApp/CadDevTools/MainWindow.xaml.vb
+++ VbNetApp/CadDevTools/MainWindow.xaml.vb
@@ -1,12 +1,22 @@
-﻿Class MainWindow
+﻿Imports System.IO
+Imports cadwiki.NetUtils
+
+Class MainWindow
     Inherits Window
     Public Sub New()
         ' This call is required by the designer.
 
         Me.Hide()
         InitializeComponent()
-        Dim window As Window = New cadwiki.CadDevTools.MainWindow()
+        Dim solutionDir As String = Paths.TryGetSolutionDirectoryPath()
+        Dim wildCardFileName As String = "*" + "MainApp.dll"
+        Dim mainAppDll As String = Paths.GetNewestDllInVsubfoldersOfSolutionDirectory(solutionDir, wildCardFileName)
+        Dim dependencies As New cadwiki.CadDevTools.MainWindow.Dependencies()
+        dependencies.DllFilePathToNetload = mainAppDll
+        Dim window As Window = New cadwiki.CadDevTools.MainWindow(dependencies)
         window.ShowDialog()
         Me.Close()
     End Sub
+
+
 End Class
