10e00a2a7b33e1ee60ac05236bc394cb85d4a568
cadwiki <99513485+cadwiki@users.noreply.github.com>
2022-11-01 20:35:48 -0700

    update refs to 1.0.10.2033

--- CSharpApp/buildThisFirst/My Project/AssemblyInfo.cs
+++ CSharpApp/buildThisFirst/My Project/AssemblyInfo.cs
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // <Assembly: AssemblyVersion("1.0.*")>
 
-[assembly: AssemblyVersion("1.0.1029.2")]
-[assembly: AssemblyFileVersion("1.0.1029.2")]
+[assembly: AssemblyVersion("1.0.1101.12")]
+[assembly: AssemblyFileVersion("1.0.1101.12")]
