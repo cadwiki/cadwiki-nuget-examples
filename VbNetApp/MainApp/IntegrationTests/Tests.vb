491df54b3cc2e5df5d2252f300adb57e6f9a33bc
cadwiki <cadwiki.net@gmail.com>
2022-12-10 15:37:37 -0800

    Update Tests.vb

--- VbNetApp/MainApp/IntegrationTests/Tests.vb
+++ VbNetApp/MainApp/IntegrationTests/Tests.vb
@@ -1,31 +1,128 @@
-﻿Option Strict On
-Option Infer Off
-Option Explicit On
+﻿Imports Autodesk.Windows
+Imports Microsoft.VisualBasic
 
 Imports NUnit.Framework
+Imports System
+Imports System.Windows.Automation
+Imports System.Windows.Automation.AutomationElement
+Imports Application = System.Windows.Forms.Application
+Imports Microsoft.Test.Input
+Imports System.Drawing.Imaging
+Imports cadwiki.NUnitTestRunner.TestEvidence
+Imports cadwiki.NUnitTestRunner.Creators
+Imports System.Threading.Tasks
+Imports System.Collections.Generic
+Imports System.IO
 
-Namespace IntegrationTests
+Namespace MainApp.IntegrationTests
     <TestFixture>
     Public Class Tests
 
         <SetUp>
         Public Sub Init()
-            Autodesk.AutoCAD.ApplicationServices.Application.DocumentManager.MdiActiveDocument.SendStringToExecute("(vla-startundomark (vla-get-ActiveDocument (vlax-get-acad-object)))" + vbLf, True, False, False)
+            Autodesk.AutoCAD.ApplicationServices.Core.Application.DocumentManager.MdiActiveDocument.SendStringToExecute("(vla-startundomark (vla-get-ActiveDocument (vlax-get-acad-object)))" & vbLf, True, False, False)
         End Sub
 
         <TearDown>
         Public Sub TearDown()
-            Autodesk.AutoCAD.ApplicationServices.Application.DocumentManager.MdiActiveDocument.SendStringToExecute("(vla-endundomark (vla-get-ActiveDocument (vlax-get-acad-object)))" + vbLf, True, False, False)
-            Autodesk.AutoCAD.ApplicationServices.Application.DocumentManager.MdiActiveDocument.SendStringToExecute("(command-s ""._undo"" ""back"" ""yes"")" + vbLf, True, False, False)
-
-
+            Autodesk.AutoCAD.ApplicationServices.Core.Application.DocumentManager.MdiActiveDocument.SendStringToExecute("(vla-endundomark (vla-get-ActiveDocument (vlax-get-acad-object)))" & vbLf, True, False, False)
+            Autodesk.AutoCAD.ApplicationServices.Core.Application.DocumentManager.MdiActiveDocument.SendStringToExecute("(command-s ""._undo"" ""back"" ""yes"")" & vbLf, True, False, False)
         End Sub
 
         <Test>
-        Public Sub Test_1()
+        Public Sub Test_Is1EqualTo1_ShouldPass()
             Assert.AreEqual(1, 1, "Test failed")
         End Sub
 
 
+
+
+        <Test>
+        Public Sub Test_ClickUiRibbonHelloWorld_ShouldAddScreenshotToPdf()
+            Dim ribbonControl = ComponentManager.Ribbon
+            Dim appTab As RibbonTab = ribbonControl.FindTab("AppTab")
+            Dim examplePanel As RibbonPanel = appTab.FindPanel(UiRibbon.Panels.Example.Id)
+            Dim item As RibbonItem = examplePanel.FindItem(UiRibbon.Panels.ExampleButtons.HelloButtonId)
+            Dim ribbonButton As RibbonButton = CType(item, RibbonButton)
+            'simulate a Ui click by calling Execute on the Ribbon button command handler
+            ribbonButton.CommandHandler.Execute(ribbonButton)
+            Call Windows.Forms.Application.DoEvents()
+
+            Dim testEvidenceCreator = New TestEvidenceCreator()
+            Dim windowIntPtr As IntPtr = testEvidenceCreator.ProcessesGetHandleFromUiTitle("Hello from Cadwiki v53")
+            Dim evidence = New Evidence()
+            testEvidenceCreator.TakeJpegScreenshot(windowIntPtr, "Title")
+            Dim controlName = "ButtonOk"
+            testEvidenceCreator.MicrosoftTestClickUiControl(windowIntPtr, controlName)
+
+            evidence = testEvidenceCreator.GetEvidenceForCurrentTest()
+            Assert.IsTrue(File.Exists(evidence.Images(0).FilePath), "jpeg was not created.")
+
+            Assert.AreEqual(1, 1, "Test failed")
+        End Sub
+
+        <Test>
+        Public Sub Test_ClickUiRibbonHelloWorld_ShouldAddSecondScreenShotToPdf()
+            Dim ribbonControl = ComponentManager.Ribbon
+            Dim appTab As RibbonTab = ribbonControl.FindTab("AppTab")
+            Dim examplePanel As RibbonPanel = appTab.FindPanel(UiRibbon.Panels.Example.Id)
+            Dim item As RibbonItem = examplePanel.FindItem(UiRibbon.Panels.ExampleButtons.HelloButtonId)
+            Dim ribbonButton As RibbonButton = CType(item, RibbonButton)
+            'simulate a Ui click by calling Execute on the Ribbon button command handler
+            ribbonButton.CommandHandler.Execute(ribbonButton)
+            Call Windows.Forms.Application.DoEvents()
+
+            Dim testEvidenceCreator = New TestEvidenceCreator()
+            Dim windowIntPtr As IntPtr = testEvidenceCreator.ProcessesGetHandleFromUiTitle("Hello from Cadwiki v53")
+            testEvidenceCreator.TakeJpegScreenshot(windowIntPtr, "Title")
+            Dim controlName = "ButtonOk"
+            testEvidenceCreator.MicrosoftTestClickUiControl(windowIntPtr, controlName)
+            Dim evidence = testEvidenceCreator.GetEvidenceForCurrentTest()
+            Assert.IsTrue(File.Exists(evidence.Images(0).FilePath), "jpeg was not created.")
+        End Sub
+
+        <Test>
+        Public Async Function Test_LongRunningLineDraw_ShouldAddSecondScreenShotToPdf() As Task(Of Object)
+            Await DelayedWork()
+            Dim doc = Autodesk.AutoCAD.ApplicationServices.Core.Application.DocumentManager.MdiActiveDocument
+            Dim parameters As List(Of Object) = New List(Of Object)() From {
+    "_.LINE",
+    "0,0",
+    "1,1",
+    ""
+}
+
+            Await Autodesk.AutoCAD.ApplicationServices.Core.Application.DocumentManager.ExecuteInCommandContextAsync(Async Function(obj)
+                                                                                                                         Await doc.Editor.CommandAsync(parameters.ToArray())
+                                                                                                                     End Function, Nothing)
+
+            parameters = New List(Of Object)() From {
+    "_.ZOOM",
+    "EXTENTS"
+}
+
+            Await Autodesk.AutoCAD.ApplicationServices.Core.Application.DocumentManager.ExecuteInCommandContextAsync(Async Function(obj)
+                                                                                                                         Await doc.Editor.CommandAsync(parameters.ToArray())
+                                                                                                                     End Function, Nothing)
+
+            Dim testEvidenceCreator = New TestEvidenceCreator()
+            Dim windowIntPtr As IntPtr = testEvidenceCreator.ProcessesGetHandleFromUiTitle("Autodesk AutoCAD")
+            testEvidenceCreator.TakeJpegScreenshot(windowIntPtr, "After draw line async")
+            Dim evidence = testEvidenceCreator.GetEvidenceForCurrentTest()
+            Assert.IsTrue(File.Exists(evidence.Images(0).FilePath), "jpeg was not created.")
+            Return Nothing
+        End Function
+
+        Private Async Function DelayedWork() As Task
+            Await Task.Delay(5000)
+        End Function
     End Class
+
+
+
+
+
+
 End Namespace
+
+
