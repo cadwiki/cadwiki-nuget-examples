0dd4ee1348abd726303fb333f07e061c8ee590d2
cadwiki <99513485+cadwiki@users.noreply.github.com>
2023-04-23 09:51:40 -0700

    update package to 1.1.5.938

--- CSharpApp/buildThisFirst/My Project/AssemblyInfo.cs
+++ CSharpApp/buildThisFirst/My Project/AssemblyInfo.cs
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // <Assembly: AssemblyVersion("1.0.*")>
 
-[assembly: AssemblyVersion("1.0.1214.33")]
-[assembly: AssemblyFileVersion("1.0.1214.33")]
+[assembly: AssemblyVersion("1.0.0423.2")]
+[assembly: AssemblyFileVersion("1.0.0423.2")]
