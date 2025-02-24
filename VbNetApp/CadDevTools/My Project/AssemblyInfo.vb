615e519aa4bb288a467bf6732d7192ae4b2b48dd
Alex <cadwiki.net@gmail.com>
2025-02-23 16:03:09 -0800

    chore: update vbnet app

--- VbNetApp/CadDevTools/My Project/AssemblyInfo.vb
+++ VbNetApp/CadDevTools/My Project/AssemblyInfo.vb
@@ -50,5 +50,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")>
 
-<Assembly: AssemblyVersion("1.0.0104.1")>
-<Assembly: AssemblyFileVersion("1.0.0104.1")>
+<Assembly: AssemblyVersion("1.0.0223.1")>
+<Assembly: AssemblyFileVersion("1.0.0223.1")>
