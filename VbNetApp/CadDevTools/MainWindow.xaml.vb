5ea4231e9ea173835d4f5eea09ffcee5a970252e
cadwiki <99513485+cadwiki@users.noreply.github.com>
2025-02-23 16:04:10 -0800

    4.0.0.2 External config (#3)
    * feat: add startup params and sample button
    
    * feat: upgrade to 4.0.0.2
    
    * chore: update vbnet app

--- VbNetApp/CadDevTools/MainWindow.xaml.vb
+++ VbNetApp/CadDevTools/MainWindow.xaml.vb
@@ -12,7 +12,7 @@ Class MainWindow
         Dim wildCardFileName As String = "*" + "MainApp.dll"
         Dim mainAppDll As String = Paths.GetNewestDllInVsubfoldersOfSolutionDirectory(solutionDir, wildCardFileName)
         Dim dependencies As New cadwiki.CadDevTools.MainWindow.Dependencies()
-        dependencies.DllFilePathToNetload = mainAppDll
+        dependencies.DllFilePathsToNetloadCommaDelimited = mainAppDll
         Dim window As Window = New cadwiki.CadDevTools.MainWindow(dependencies)
         window.Show()
     End Sub
